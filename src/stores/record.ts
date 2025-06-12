8f67613f6cbe62c182655d2de69325cdf1071b73
serj2626 <serj2626@mail.ru>
2025-06-12 18:33:43 +0300

    rename store

--- src/stores/record.ts
+++ src/stores/record.ts
@@ -1,7 +1,8 @@
-import { ref, computed } from 'vue'
+import { computed, ref } from 'vue'
+
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
+export const useRecordStore = defineStore('record', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
   function increment() {
